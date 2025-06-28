030729fda32cd71f8be49745a6eb0ec3c2a3e227
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 01:15:50 -0300

    feat: add Multicopter controller

--- controladores/projetarControladorMulticoptero.m
+++ controladores/projetarControladorMulticoptero.m
@@ -1,1 +1,11 @@
 % Função para o projeto do controlador de Multicoptero
+
+function controlador = projetarControladorMulticoptero(requisitos, planta)
+
+controlador.theta = projetarControladorArfagem(requisitos.theta, planta);
+controlador.phi = projetarControladorRolagem(requisitos.phi, planta);
+controlador.x = projetarControladorHorizontalBusca(requisitos.x, requisitos.theta, planta);
+controlador.y = projetarControladorHorizontalBusca(requisitos.y, requisitos.phi, planta);
+controlador.z = projetarControladorVerticalBusca(requisitos.z, planta);
+
+end
