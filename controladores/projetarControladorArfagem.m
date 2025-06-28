ed387108296ad0c7700024c02cff68575db5a942
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 00:38:37 -0300

    feat: add pitch controller

--- controladores/projetarControladorArfagem.m
+++ controladores/projetarControladorArfagem.m
@@ -1,1 +1,10 @@
 % Função para o projeto do controlador de Arfagem
+
+function controlador = projetarControladorArfagem(requisitos, planta)
+
+xi = sqrt((log(requisitos.Mp))^2 / (pi^2 + (log(requisitos.Mp))^2));
+wn = (pi - acos(xi)) / (requisitos.tr * sqrt(1 - xi^2));
+controlador.Kv = 2 * wn * xi * planta.J;
+controlador.Kp = wn / (2 * xi);
+
+end
