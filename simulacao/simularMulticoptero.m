6b06f08f9447c17a1ca1be9678a0f4137a8073e7
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 01:49:19 -0300

    feat: add simulink simulation configuration

--- simulacao/simularMulticoptero.m
+++ simulacao/simularMulticoptero.m
@@ -1,1 +1,31 @@
 % Simulação do sistema Multicoptero
+
+function simulacao = simularMulticoptero(controlador, planta, tf, xr, yr, zr,...
+    carga, ventoX)
+controlador.m = planta.m;
+controlador.g = planta.g;
+
+% Configurando as variaveis usadas no Simulink
+assignin('base', 'xr', xr);
+assignin('base', 'yr', yr);
+assignin('base', 'zr', zr);
+assignin('base', 'x0', 0);
+assignin('base', 'y0', 0);
+assignin('base', 'z0', 0);
+assignin('base', 'theta0', 0);
+assignin('base', 'phi0', 0);
+assignin('base', 'controlador', controlador);
+assignin('base', 'planta', planta);
+assignin('base', 'carga', carga);
+assignin('base', 'ventoX', ventoX);
+
+% Carregando o Simulink
+load_system('multicoptero');
+
+% Configurando o tempo final de simulacao
+set_param('multicoptero', 'StopTime', sprintf('%g', tf));
+
+% Rodando a simulacao
+simulacao = sim('multicoptero');
+
+end
