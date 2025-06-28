546f5a83eea5e8bd10c675580f3dfa112aa4541f
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 01:12:16 -0300

    feat: add analytical vertical controller

--- controladores/projetarControladorVerticalAnalitico.m
+++ controladores/projetarControladorVerticalAnalitico.m
@@ -1,1 +1,10 @@
 % Função para o projeto do controlador de Vertical Analitico
+
+function controlador = projetarControladorVerticalAnalitico(requisitos, planta)
+
+xi = sqrt((log(requisitos.Mp))^2 / (pi^2 + (log(requisitos.Mp))^2));
+wn = (pi - acos(xi)) / (requisitos.tr * sqrt(1 - xi^2));
+controlador.Kd = 7 * planta.m * xi * wn;
+controlador.Kp = wn^2 * planta.m * (1 + 10 * xi^2);
+controlador.Ki = 5 * planta.m * xi * wn^3;
+end
