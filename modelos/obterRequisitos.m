179a962e0d394ff1e076c76c35214f4abb943667
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 00:29:29 -0300

    feat: add requirements

--- modelos/obterRequisitos.m
+++ modelos/obterRequisitos.m
@@ -1,1 +1,15 @@
 % Modelagem da planta e estruturas de requisitos
+function requisitos = obterRequisitos()
+
+requisitos.x.tr = 1.0;
+requisitos.x.Mp = 0.1;
+requisitos.y.tr = 1.0;
+requisitos.y.Mp = 0.1;
+requisitos.z.tr = 1.0;
+requisitos.z.Mp = 0.1;
+requisitos.theta.tr = 0.1;
+requisitos.theta.Mp = 0.05;
+requisitos.phi.tr = 0.1;
+requisitos.phi.Mp = 0.05;
+
+end
