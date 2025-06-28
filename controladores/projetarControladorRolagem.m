867f6cd680c3a4b454a27a7660a0154fc5c88301
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-28 00:47:21 -0300

    feat: add roll controller

--- controladores/projetarControladorRolagem.m
+++ controladores/projetarControladorRolagem.m
@@ -1,1 +1,9 @@
-% Função para o projeto do controlador de Rolagem
+% Função para o projeto do controlador de Rolamento
+
+function controlador = projetarControladorRolagem(requisitos, planta)
+
+xi = sqrt((log(requisitos.Mp))^2 / (pi^2 + (log(requisitos.Mp))^2));
+wn = (pi - acos(xi)) / (requisitos.tr * sqrt(1 - xi^2));
+controlador.Kv = 2 * wn * xi * planta.J;
+controlador.Kp = wn / (2 * xi);
+end
