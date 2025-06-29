2868b7984140542a40390aa7a7b43109bcc0e03b
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-06-29 01:01:09 -0300

    feat: add yaw controller function for multicopter

--- controladores/projetarControladorMulticoptero.m
+++ controladores/projetarControladorMulticoptero.m
@@ -4,6 +4,7 @@ function controlador = projetarControladorMulticoptero(requisitos, planta)
 
 controlador.theta = projetarControladorArfagem(requisitos.theta, planta);
 controlador.phi = projetarControladorRolagem(requisitos.phi, planta);
+controlador.psi = projetarControladorGuinada(requisitos.psi, planta);
 controlador.x = projetarControladorHorizontalBusca(requisitos.x, requisitos.theta, planta);
 controlador.y = projetarControladorHorizontalBusca(requisitos.y, requisitos.phi, planta);
 controlador.z = projetarControladorVerticalBusca(requisitos.z, planta);
