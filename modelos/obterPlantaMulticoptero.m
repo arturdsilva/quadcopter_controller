2d415e36e32cfdc46965b0eef3d8f76981f2bb27
Guilherme Moriya <guicrfmoriya@gmail.com>
2025-07-02 00:53:00 -0300

    feat: add plant parameters

--- modelos/obterPlantaMulticoptero.m
+++ modelos/obterPlantaMulticoptero.m
@@ -4,5 +4,10 @@ planta.J = 0.04;
 planta.Jz = 0.08;
 planta.l = 0.2;
 planta.g = 9.81;
+planta.k = 2.98*10^(-6);
+planta.b = 1.14*10^(-7);
+planta.Ax = 0.25;
+planta.Ay = 0.25;
+planta.Az = 0.25;
 
 end
